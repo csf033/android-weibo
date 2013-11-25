769dd7c9850e09de6b7a50a0bc6b51b831bcbcbf
djh <djh@djh-PC>
2013-11-25 11:46:31 +0800

    hh

--- Wei/src/com/csf/wei/ui/activities/AccountManagerActivity.java
+++ Wei/src/com/csf/wei/ui/activities/AccountManagerActivity.java
@@ -1,5 +1,11 @@
 package com.csf.wei.ui.activities;
 
+import android.os.Bundle;
+
 public class AccountManagerActivity extends BaseActivity {
-	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
 }
